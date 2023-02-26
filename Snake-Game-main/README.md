c1eb04eab30f32dae399d6554f3cac542d9774c8
yashdavda789 <yashdavda789@gmail.com>
2023-02-26 18:24:42 +0530

    Initial commit

---
+++ Snake-Game-main/README.md
@@ -0,0 +1,4 @@
+# Snake-Game
+This is a simple Snake Game using JAVA.
+This project include 3 Class files. 
+This game is developed in Eclips IDE.
---
+++ Snake-Game-main/SnakeGame.java
@@ -0,0 +1,16 @@
+package snake;
+
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+//This a main Class of this Project
+
+public class SnakeGame {
+
+	public static void main(String[] args){
+
+		new HomeFrame();
+	}
+
+}
